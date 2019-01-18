4676c9d9915f7f78da198da2295ab6deeecdd9f1
amchappell9 <amchappell9@gmail.com>
2019-01-17 21:03:54 -0600

    Refactoring templates

--- src/App.js
+++ src/App.js
@@ -19,20 +19,16 @@ class App extends Component {
   }
 
   render() {
-    let mainApp;
-
-    if (!this.state.isLoggedIn) {
-      mainApp = <Login clickEvent={this.login} />;
-    } else {
-      mainApp = <NowPlaying />;
-    }
-
     return (
       <div className="App bg-black">
         <Header isLoggedIn={this.state.isLoggedIn} />
         <main>
           <div className="container mx-auto mt-4">
-            {mainApp}
+            {!this.state.isLoggedIn ? (
+              <Login clickEvent={this.login} />
+            ) : (
+              <NowPlaying />
+            )}
           </div>
         </main>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,17 @@
 import React from 'react'
 
 const Header = ({isLoggedIn}) => {
-    let header;
-
-    if (isLoggedIn) {
-        header = (
-            <header className="h-20 border-b border-black border-solid">
-                <div className="container mx-auto pt-5 text-magenta">
+    return (
+        <header className="h-20 border-b border-black border-solid">
+            <div className="container mx-auto pt-5 text-magenta">
+                {isLoggedIn ? (
                     <h1>What's That Lyric?</h1>
-                </div>
-            </header>
-        );
-    } else {
-        header = (
-            <header className="h-20 border-b border-black border-solid">
-                <div className="container mx-auto pt-5 text-magenta">
+                ) : (
                     {/* <h3>Non Logged in header</h3> */}
-                </div>
-            </header>
-        );
-    }
-
-    return header;
+                )}
+            </div>
+        </header>
+    );
 }
 
 export default Header
