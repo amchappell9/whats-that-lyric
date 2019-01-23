4ec6daada9be5bc15c33836af6eb56b7e409c8d8
amchappell9 <amchappell9@gmail.com>
2019-01-22 21:28:25 -0600

    Starting on the now playing

--- src/NowPlaying.js
+++ src/NowPlaying.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 class NowPlaying extends Component {
     render () {
         return (
-            <div className="text-magenta">
-                Now Playing
+            <div>
+                <div className="flex justify-center mt-12">
+                    <h1 className="text-grey text-3xl">Now Playing:</h1>                    
+                </div>
+                <div className="flex justify-center">
+                    <p className="text-magenta font-bold text-5xl">Goosebumps</p>
+                </div>
+                <div className="flex justify-center">
+                    <p className="text-magenta font-bold text-3xl">By Travis Scott</p>
+                </div>
             </div>
         )
     }
