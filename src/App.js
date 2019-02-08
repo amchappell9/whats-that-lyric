ce4cd0ab40dbc284e3367bb4fd6c981f1afb914a
amchappell9 <amchappell9@gmail.com>
2019-02-07 20:58:35 -0600

    Adding spotify api and getting access tokens from params

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import Header from './Header';
 import Login from './Login';
 import NowPlaying from './NowPlaying';
 import Footer from './Footer';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi;
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    const params = getHashParams();
+    const token = params.access_token;
+
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: token ? true : false, 
+      nowPlaying: { name: 'Not Checked', albumArt: ''}
     };
+  }
+
+  getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+
+    return hashParams;
   }
 
   login = () => {
