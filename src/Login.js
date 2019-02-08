02a64861e5a0c55e6109647f326fa09cc3c80beb
amchappell9 <amchappell9@gmail.com>
2019-02-07 20:32:30 -0600

    Started on login button, changed button to link

--- src/Login.js
+++ src/Login.js
@@ -7,8 +7,9 @@ class Login extends Component {
                 <div className="flex justify-center text-5xl">
                     <h1>What's That Lyric?</h1>
                 </div>
-                <div className="flex justify-center mt-32">
-                    <button onClick={this.props.clickEvent} className="border-4 border-magenta text-magenta text-2xl font-bold px-24 py-8 hover:text-black hover:bg-magenta hover:border-black">Login to Spotify</button>
+                <div className="flex justify-center mt-24">
+                    {/* <button onClick={this.props.clickEvent} className="border-4 border-magenta text-magenta text-2xl font-bold px-24 py-8 hover:text-black hover:bg-magenta hover:border-black">Login to Spotify</button> */}
+                    <a href="http://localhost:8888" className="border-4 border-magenta text-magenta text-3xl font-bold px-16 py-6 hover:text-black hover:bg-magenta hover:border-black no-underline">Login</a>
                 </div>
             </div>
         )
