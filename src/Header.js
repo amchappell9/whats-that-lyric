d3ee453c2edca0a48a33ea50f7f9119019b2a719
amchappell9 <amchappell9@gmail.com>
2019-03-06 20:30:10 -0600

    Adding fancy border to the top

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = ({ isLoggedIn, handleLogout}) => {
     return (
-        <header className="h-20 border-b border-black border-solid">
+        <header className="h-20 border-t-4 border-magenta border-solid">
             <div className="container mx-auto pt-5 text-magenta">
                 {isLoggedIn && (
                     <div className="flex justify-between">
