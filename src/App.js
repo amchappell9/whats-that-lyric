3977453a7422c57fedd8e14faf0f1c8b95e0faa4
amchappell9 <amchappell9@gmail.com>
2020-03-19 20:44:57 -0500

    Rewrote App.js to use hooks. Moved old logic to App_Old.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import './App.css';
-import Header from './Header';
-import Login from './Login';
-import NowPlaying from './NowPlaying';
-import Footer from './Footer';
+import PropTypes from 'prop-types';
 import SpotifyWebApi from 'spotify-web-api-js';
-import NothingPlaying from './NothingPlaying';
-import SongInfo from './SongInfo';
 
-const redirect_uri = 'http://localhost:3000';
+import Header from './Header';
+import Footer from './Footer';
+import Login from './Login';
+import NothingPlaying from './NothingPlaying';
+import NowPlaying from './NowPlaying';
+import SongInfo from './SongInfo';
 
 const spotifyApi = new SpotifyWebApi();
 const spotifyClientId = 'a0cb7862b26747b2912a710a0d98b39a';
 const spotifyScope = 'user-read-playback-state';
 const spotifyStateKey = 'spotify_auth_state';
+const redirect_uri = 'http://localhost:3000';
 
 // API calls to genius can be made like this: api.genius.com/search?q=Kendrick%20Lamar&access_token=WOfEtjZLwyar4cFto_v8bpb9ieA_YVbTxVJwcs7AW9yJbAuUe8La3FKSazhtIbJg
 // const geniusClientId = 'JmGJFDuspgC_KGbVc340ZrWrLMKVlKUx9S3BGfhBQT65EyZPxSlokCWy3fI9onRA';
@@ -41,130 +41,91 @@ const NowPlayingSubGrid = styled.div`
   grid-template-rows: 2fr;
 `;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const getHashParams = () => {
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  e = r.exec(q);
+  while (e) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+    e = r.exec(q);
+  }
 
-    const params = this.getHashParams();
-    const token = params.access_token;
+  return hashParams;
+};
 
+const generateRandomString = length => {
+  var text = '';
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
+
+const redirectToSpotifyAuth = () => {
+  // Build spotify url and redirect to it
+  var state = generateRandomString(16);
+
+  localStorage.setItem(spotifyStateKey, state);
+
+  var url = 'https://accounts.spotify.com/authorize';
+  url += '?response_type=token';
+  url += '&client_id=' + encodeURIComponent(spotifyClientId);
+  url += '&scope=' + encodeURIComponent(spotifyScope);
+  url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+  url += '&state=' + encodeURIComponent(state);
+
+  window.location = url;
+};
+
+const handleLogout = () => {
+  window.location = window.location.href.substr(
+    0,
+    window.location.href.indexOf('#')
+  );
+};
+
+const App = () => {
+  const params = getHashParams();
+  const token = params.access_token;
+  const isAuthenticated = token ? true : false;
+
+  const [nowPlaying, setNowPlaying] = useState(null);
+  const [geniusInfo, setGeniusInfo] = useState(null);
+  const [nothingPlaying, setNothingPlaying] = useState(false);
+
+  useEffect(() => {
     if (token) {
       spotifyApi.setAccessToken(token);
-      this.getNowPlaying();
+
+      getNowPlaying();
     }
+  }, [token]);
 
-    this.state = {
-      isLoggedIn: token ? true : false,
-      nowPlaying: {
-        songName: 'Not Checked',
-        artists: [],
-        albumArt: ''
-      },
-      geniusSongInfo: null,
-      nothingPlaying: false
-    };
-  }
-
-  componentDidMount() {
-    document.addEventListener(
-      'visibilitychange',
-      this.handleTabBackInFocus,
-      false
-    );
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener(
-      'visibilitychange',
-      this.handleTabBackInFocus,
-      false
-    );
-  }
-
-  handleTabBackInFocus = () => {
-    if (this.state.isLoggedIn && !document.hidden) {
-      this.getNowPlaying();
-    }
+  const getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      if (response) {
+        setNowPlaying({
+          songName: response.item.name,
+          artists: response.item.artists,
+          albumArt: response.item.album.images[0].url
+        });
+        setNothingPlaying(false);
+        searchGeniusAPI(response.item.name, response.item.artists[0].name);
+      } else {
+        // Nothing Playing
+        setNothingPlaying(true);
+        setNowPlaying(null);
+      }
+    });
   };
 
-  getHashParams = () => {
-    var hashParams = {};
-    var e,
-      r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    e = r.exec(q);
-    while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
-    }
-
-    return hashParams;
-  };
-
-  redirectToSpotifyAuth = () => {
-    // Build spotify url and redirect to it
-    var state = this.generateRandomString(16);
-
-    localStorage.setItem(spotifyStateKey, state);
-
-    var url = 'https://accounts.spotify.com/authorize';
-    url += '?response_type=token';
-    url += '&client_id=' + encodeURIComponent(spotifyClientId);
-    url += '&scope=' + encodeURIComponent(spotifyScope);
-    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-    url += '&state=' + encodeURIComponent(state);
-
-    window.location = url;
-  };
-
-  handleLogout = () => {
-    window.location = window.location.href.substr(
-      0,
-      window.location.href.indexOf('#')
-    );
-  };
-
-  generateRandomString = length => {
-    var text = '';
-    var possible =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-  };
-
-  getNowPlaying = () => {
-    spotifyApi
-      .getMyCurrentPlaybackState()
-      .then(response => {
-        if (response) {
-          this.setState({
-            nowPlaying: {
-              songName: response.item.name,
-              artists: response.item.artists,
-              albumArt: response.item.album.images[0].url
-            }
-          });
-
-          this.searchGeniusAPI(
-            response.item.name,
-            response.item.artists[0].name
-          );
-        } else {
-          this.setState({
-            nothingPlaying: true
-          });
-        }
-      })
-      .catch(error => {
-        console.error('Error getting getting current playback song');
-      });
-  };
-
-  searchGeniusAPI = (songName, artistName) => {
+  const searchGeniusAPI = (songName, artistName) => {
     const geniusSearchURL = `https://api.genius.com/search?access_token=${geniusClientAccessToken}&q=${encodeURI(
       songName
     )} ${encodeURI(artistName)}`;
@@ -174,60 +135,47 @@ class App extends Component {
         return response.json();
       })
       .then(response => {
-        this.getGeniusSongInfo(response.response.hits[0]);
+        // We're hoping that the first result is the best one
+        getGeniusSongInfo(response.response.hits[0]);
       });
   };
 
-  getGeniusSongInfo = geniusResult => {
-    // Get URL from result
-    const lyricsURL = `https://api.genius.com${geniusResult.result.api_path}?access_token=${geniusClientAccessToken}&text_format=html`;
+  const getGeniusSongInfo = geniusSearchResult => {
+    const lyricsURL = `https://api.genius.com${geniusSearchResult.result.api_path}?access_token=${geniusClientAccessToken}&text_format=html`;
 
-    // Call endpoint
     fetch(lyricsURL)
       .then(response => {
         return response.json();
       })
       .then(response => {
-        this.setState({
-          geniusSongInfo: response.response.song
-        });
+        setGeniusInfo(response.response.song);
       });
   };
 
-  render() {
-    let content;
+  let content;
 
-    if (!this.state.isLoggedIn) {
-      content = <Login handleLogin={this.redirectToSpotifyAuth} />;
-    } else if (this.state.isLoggedIn && this.state.nothingPlaying) {
-      content = <NothingPlaying />;
-    } else if (
-      this.state.isLoggedIn &&
-      this.state.nowPlaying.name !== 'Not Checked' &&
-      this.state.geniusSongInfo
-    ) {
-      content = (
-        <NowPlayingSubGrid>
-          <NowPlaying
-            nowPlaying={this.state.nowPlaying}
-            songInfo={this.state.geniusSongInfo}
-          />
-          <SongInfo songInfo={this.state.geniusSongInfo} />
-        </NowPlayingSubGrid>
-      );
-    }
-
-    return (
-      <StyledApp>
-        <Header
-          isLoggedIn={this.state.isLoggedIn}
-          handleLogout={this.handleLogout}
-        />
-        <StyledMain>{content}</StyledMain>
-        <Footer />
-      </StyledApp>
+  if (!isAuthenticated) {
+    content = <Login handleLogin={redirectToSpotifyAuth} />;
+  } else if (isAuthenticated && nothingPlaying) {
+    content = <NothingPlaying />;
+  } else if (isAuthenticated && nowPlaying && geniusInfo) {
+    content = (
+      <NowPlayingSubGrid>
+        <NowPlaying nowPlaying={nowPlaying} songInfo={geniusInfo} />
+        <SongInfo songInfo={geniusInfo} />
+      </NowPlayingSubGrid>
     );
   }
-}
+
+  return (
+    <StyledApp>
+      <Header isLoggedIn={isAuthenticated} handleLogout={handleLogout} />
+      <StyledMain>{content}</StyledMain>
+      <Footer />
+    </StyledApp>
+  );
+};
+
+App.propTypes = {};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Reset } from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 import './index.css';
+// import App_Old from './App_Old';
 import App from './App';
-import App_New from './App_New';
 import * as serviceWorker from './serviceWorker';
 
 const GlobalStyle = createGlobalStyle`
@@ -21,8 +21,8 @@ ReactDOM.render(
   <>
     <Reset />
     <GlobalStyle />
-    {/* <App /> */}
-    <App_New />
+    {/* <App_Old /> */}
+    <App />
   </>,
   document.getElementById('root')
 );
